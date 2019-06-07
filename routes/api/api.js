c4004f6401de2331c76436dd353a3aad33e0d50b
valgon <govajosue@protonmail.ch>
2019-06-06 19:32:57 -0600

    Imagenes Finales

--- routes/api/api.js
+++ routes/api/api.js
@@ -3,10 +3,6 @@ const router = express.Router();
 
 const InfoApi = require('./Info');
 
-/*router.get('/', (req, res, next)=>{
-    res.status(200).json({"api":"Prueba 1."})
-});*/
-
 router.use('/Info', InfoApi);
 
 module.exports = router;
